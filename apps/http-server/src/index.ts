4cbf101527a5d87cb7a9d45e1c2b5b553735c067
Prince Saliya <princesaliya4040@gmail.com>
2026-03-30 18:28:33 +0530

    lefthook changes

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,5 +1,6 @@
 import { appRouter } from "@canvio/trpc";
-import { createHTTPServer } from "@trpc/server/adapters/standalone";
+
+// import { createHTTPServer } from "@trpc/server/adapters/standalone";
 
 const server = createHTTPServer({
 	router: appRouter,
