94e017d4b35631a0f96539d211ca892225a1f527
Prince Saliya <princesaliya4040@gmail.com>
2026-03-31 10:20:44 +0530

    added jwt util

--- apps/http-server/src/context.ts
+++ apps/http-server/src/context.ts
@@ -1,7 +1,10 @@
 import { createContextInner } from "@canvio/trpc/context";
 import type { CreateHTTPContextOptions } from "@trpc/server/adapters/standalone";
+import { parse } from "cookie";
 import { prisma } from "./prisma";
 
 export function createContext(opts: CreateHTTPContextOptions) {
+	const parsedCookies = parse(String(opts.req.headers.cookie));
+	console.log(parsedCookies);
 	return createContextInner({ prisma });
 }
