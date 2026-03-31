bda437d22c1dc7b51590661cb9ec7bb3f5dbd375
Prince Saliya <princesaliya4040@gmail.com>
2026-03-31 10:55:01 +0530

    refactor: simplify user context handling in createContext functions

--- apps/http-server/src/context.ts
+++ apps/http-server/src/context.ts
@@ -1,5 +1,4 @@
 import { createContextInner } from "@canvio/trpc/context";
-import { TRPCError } from "@trpc/server";
 import type { CreateHTTPContextOptions } from "@trpc/server/adapters/standalone";
 import { parse } from "cookie";
 import { prisma } from "./prisma";
@@ -9,29 +8,27 @@ export async function createContext(opts: CreateHTTPContextOptions) {
 	const parsedCookies = parse(String(opts.req.headers.cookie));
 	const token = parsedCookies.token;
 
-	if (!token) {
-		throw new TRPCError({
-			code: "UNAUTHORIZED",
-			message: "token not found",
-		});
-	}
+	let userId = null;
+	let role = null;
 
-	const [jwtError, jwtResult] = await withCatch(JwtService.verify(token));
-	if (jwtError) {
-		throw new TRPCError({
-			code: "FORBIDDEN",
-			message: "user is not authorised",
-		});
-	}
+	if (token) {
+		const [_, jwtResult] = await withCatch(JwtService.verify(token));
 
-	const userId = jwtResult.payload.userId;
-	const role = jwtResult.payload.role;
+		userId = jwtResult?.payload.userId;
+		role = jwtResult?.payload.role;
+	}
 
 	return createContextInner({
 		prisma,
-		user: {
-			id: userId,
-			role,
-		},
+		...(userId && role
+			? {
+					user: {
+						id: userId,
+						role,
+					},
+				}
+			: {
+					user: null,
+				}),
 	});
 }
--- packages/trpc/src/context.ts
+++ packages/trpc/src/context.ts
@@ -5,16 +5,22 @@ type createContextInnerOpts = {
 	user: {
 		id: string;
 		role: "admin" | "viewer" | "writer";
-	};
+	} | null;
 };
 
 export async function createContextInner(opts: createContextInnerOpts) {
 	return {
 		prisma: opts.prisma,
-		user: {
-			id: opts.user.id,
-			role: opts.user.role,
-		},
+		...(opts.user
+			? {
+					user: {
+						id: opts.user.id,
+						role: opts.user.role,
+					},
+				}
+			: {
+					user: null,
+				}),
 	};
 }
 
