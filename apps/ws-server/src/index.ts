2a47558352f50b3db0aed2351664981296b188e1
Prince Saliya <princesaliya4040@gmail.com>
2026-04-06 17:22:15 +0530

    changes

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,13 +1,27 @@
+import { withCatch } from "@canvio/util/withCatch";
 import "@canvio/env/loader";
 import { wsEnv } from "@canvio/env/ws-server";
 import { parse } from "cookie";
 import { WebSocketServer } from "ws";
+import { ConnectionManger } from "./service/connectionManager";
+import { JwtService } from "./utils";
 
 const wss = new WebSocketServer({ port: wsEnv.WS_PORT });
 
-wss.on("connection", (socket, request) => {
+wss.on("connection", async (socket, request) => {
 	const cookies = request.headers.cookie;
 	const parsedCookies = parse(cookies as string);
 	const token = parsedCookies.token;
-	console.log(token);
+
+	if (!token) return;
+
+	const [jwtError, jwtResult] = await withCatch(JwtService.verify(token));
+
+	if (jwtError) {
+		return;
+	}
+
+	const userId = jwtResult.payload.userId;
+
+	const _ = new ConnectionManger(socket, userId);
 });
---
+++ apps/ws-server/src/utils/index.ts
@@ -0,0 +1,11 @@
+import { serverEnv } from "@canvio/env/server";
+import type { JwtPayload } from "@canvio/util/auth";
+import { JwtUtil } from "@canvio/util/jwt";
+
+export { withCatch } from "@canvio/util/withCatch";
+
+export const JwtService = new JwtUtil<JwtPayload>({
+	secret: serverEnv.JWT_SECRET,
+	issuer: "canvio",
+	expirationTime: "2d",
+});
