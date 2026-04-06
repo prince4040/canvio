b3092c58e14c8d9cb7153d3b15fa5c6af687be94
Prince Saliya <princesaliya4040@gmail.com>
2026-04-06 17:01:21 +0530

    web socket changes

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,7 +1,13 @@
 import "@canvio/env/loader";
 import { wsEnv } from "@canvio/env/ws-server";
+import { parse } from "cookie";
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: wsEnv.WS_PORT });
 
-wss.on("connection", (socket) => {});
+wss.on("connection", (socket, request) => {
+	const cookies = request.headers.cookie;
+	const parsedCookies = parse(cookies as string);
+	const token = parsedCookies.token;
+	console.log(token);
+});
